b318137f0236be9ee2793732d13b28963cf13023
fuentes29 <fuentes102002@gmail.com>
2024-05-16 10:43:37 -0500

    corregi la variable de passworencoder

--- TidyDay-Project/src/main/java/com/tidyday/TidyDay/Project/controller/AuthController.java
+++ TidyDay-Project/src/main/java/com/tidyday/TidyDay/Project/controller/AuthController.java
@@ -7,6 +7,7 @@ import com.tidyday.TidyDay.Project.service.CustomerUserDetailslmpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     @Autowired
     private CustomerUserDetailslmpl customeUserDetails;
 
