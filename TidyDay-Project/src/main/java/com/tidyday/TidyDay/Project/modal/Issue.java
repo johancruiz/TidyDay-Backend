61b09009af3017f276616d82d56b75e5968902bb
johancruiz <106390573+johancruiz@users.noreply.github.com>
2024-05-20 11:08:49 -0500

    update

--- TidyDay-Project/src/main/java/com/tidyday/TidyDay/Project/modal/Issue.java
+++ TidyDay-Project/src/main/java/com/tidyday/TidyDay/Project/modal/Issue.java
@@ -3,12 +3,24 @@ package com.tidyday.TidyDay.Project.modal;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Data
 public class Issue {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    private String title;
+    private String description;
+    private String status;
+    private Long scheduleId;
+    private String priority;
+    private LocalDate dueDate;
+    private List<String> tags = new ArrayList<>();
 
     @ManyToOne
     private User assignee;
