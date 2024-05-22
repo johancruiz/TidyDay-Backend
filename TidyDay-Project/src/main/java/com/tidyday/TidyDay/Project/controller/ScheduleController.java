e1d802695326e5730f3cbf889a6d7208ed6bd2bb
fuentes29 <fuentes102002@gmail.com>
2024-05-22 10:01:46 -0500

    Summary All

--- TidyDay-Project/src/main/java/com/tidyday/TidyDay/Project/controller/ScheduleController.java
+++ TidyDay-Project/src/main/java/com/tidyday/TidyDay/Project/controller/ScheduleController.java
@@ -1,8 +1,11 @@
 package com.tidyday.TidyDay.Project.controller;
 
+import com.tidyday.TidyDay.Project.modal.Chat;
 import com.tidyday.TidyDay.Project.modal.Schedule;
 import com.tidyday.TidyDay.Project.modal.User;
+import com.tidyday.TidyDay.Project.response.MessageResponse;
 import com.tidyday.TidyDay.Project.service.ScheduleService;
+import com.tidyday.TidyDay.Project.service.UserService;
 import io.jsonwebtoken.Jwt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -26,7 +29,7 @@ public class ScheduleController {
             @RequestParam(required = false)String tag,
             @RequestHeader("Authorization") String jwt
 
-    ){
+    )throws Exception {
         User user=userService.findUserProfileJwt(Jwt);
         List<Schedule> Schedules=scheduleService.getScheduleByTeam(user,category,tag);
         return new ResponseEntity<>(Schedules, HttpStatus.OK);
@@ -37,7 +40,7 @@ public class ScheduleController {
            @PathVariable Long ScheduleId,
             @RequestHeader("Authorization") String jwt
 
-    ){
+    )throws  Exception {
         User user=userService.findUserProfileJwt(Jwt);
         Schedule schedule=scheduleService.getScheduleById(ScheduleId);
         return new ResponseEntity<>(schedule, HttpStatus.OK);
@@ -48,7 +51,7 @@ public class ScheduleController {
             @RequestHeader("Authorization") String jwt,
             @RequestBody Schedule schedule
 
-    ){
+    )throws  Exception {
         User user=userService.findUserProfileJwt(Jwt);
         Schedule createdschedule=scheduleService.createSchedule(schedule,user);
         return new ResponseEntity<>(createdschedule, HttpStatus.OK);
@@ -60,20 +63,44 @@ public class ScheduleController {
             @RequestHeader("Authorization") String jwt,
             @RequestBody Schedule schedule
 
-    ){
+    )throws Exception {
         User user=userService.findUserProfileJwt(Jwt);
         Schedule updateschedule=scheduleService.updateSchedule(schedule,ScheduleId);
         return new ResponseEntity<>(updateschedule, HttpStatus.OK);
     }
 
     @DeleteMapping("/{scheduleId}")
-    public ResponseEntity<Schedule>deleteschedule(
+    public ResponseEntity<MessageResponse>deleteschedule(
             @PathVariable Long ScheduleId,
-            @RequestHeader("Authorization") String jwt,
+            @RequestHeader("Authorization") String jwt
 
-    ){
+    )throws Exception {
         User user=userService.findUserProfileJwt(Jwt);
         scheduleService.deleteSchedule(ScheduleId,user.getId());
-        return new ResponseEntity<>(deleteschedule, HttpStatus.OK);
+        MessageResponse res= new MessageResponse("Schedule deleted succesfully");
+        return new ResponseEntity<>(res, HttpStatus.OK);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Schedule>> searchSchedules (
+            @RequestParam(required = false)String keyword,
+
+            @RequestHeader("Authorization") String jwt
+
+    )throws Exception {
+        User user =userService.findUserProfileByJwt(Jwt);
+        List<Schedule> Schedules=scheduleService.searchSchedules(keyword,user);
+        return new ResponseEntity<>(Schedules, HttpStatus.OK);
+    }
+
+    @GetMapping("/{ScheduleId}/chat")
+    public ResponseEntity<Chat>getChatByScheduleId(
+            @PathVariable Long ScheduleId,
+            @RequestHeader("Authorization") String jwt
+
+    )throws  Exception {
+        User user=userService.findUserProfileJwt(Jwt);
+        Chat chat=scheduleService.getChatByScheduleId(ScheduleId);
+        return new ResponseEntity<>(chat, HttpStatus.OK);
     }
 }
---
+++ TidyDay-Project/src/main/java/com/tidyday/TidyDay/Project/response/MessageResponse.java
@@ -0,0 +1,15 @@
+package com.tidyday.TidyDay.Project.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageResponse {
+
+    private String message;
+
+
+}
