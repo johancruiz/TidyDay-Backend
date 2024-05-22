3b09882b15c18eadd9f82eb03c026493b6b783f3
johancruiz <106390573+johancruiz@users.noreply.github.com>
2024-05-22 11:05:15 -0500

    scheduleID

--- TidyDay-Project/src/main/java/com/tidyday/TidyDay/Project/modal/Issue.java
+++ TidyDay-Project/src/main/java/com/tidyday/TidyDay/Project/modal/Issue.java
@@ -19,6 +19,7 @@ public class Issue {
     private String description;
     private String status;
     private String priority;
+    private String scheduleID;
     private LocalDate dueDate;
     private List<String> tags = new ArrayList<>();
 
